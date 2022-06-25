02093fe9a424cae7721d38a073b2f9ed82a1dc9b
makifermis <makifermis@gmail.com>
2022-06-25 22:14:29 +0200

    pom.xml updated

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -14,8 +14,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/library/step_definitions",
-        dryRun = false,
-        tags = "@librarians"
+        dryRun = true,
+        tags = "@wip"
 
 )
 
